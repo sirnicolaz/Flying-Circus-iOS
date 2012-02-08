0b3a3b42e360a59ef2a24c7dfd41374848d9435a
Nicola Miotto <sirnicolaz@gmail.com>
2012-02-08 21:13:44 +0100

    Initial Commit

---
+++ FlyingCircus/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  FlyingCircus
+//
+//  Created by Nicola Miotto on 2/8/12.
+//  Copyright (c) 2012 Università degli studi di Padova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ FlyingCircus/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  FlyingCircus
+//
+//  Created by Nicola Miotto on 2/8/12.
+//  Copyright (c) 2012 Università degli studi di Padova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
