e0007eb835bb9e91a845f9cb75016aeb4bf5e6b0
Nicola Miotto <sirnicolaz@gmail.com>
2012-02-09 15:40:23 +0100

    Re-add Season model

---
+++ Season.m
@@ -0,0 +1,20 @@
+//
+//  Season.m
+//  FlyingCircus
+//
+//  Created by Nicola Miotto on 2/9/12.
+//  Copyright (c) 2012 Università degli studi di Padova. All rights reserved.
+//
+
+#import "Season.h"
+#import "Episode.h"
+
+
+@implementation Season
+
+@dynamic fromDate;
+@dynamic number;
+@dynamic toDate;
+@dynamic episodes;
+
+@end
